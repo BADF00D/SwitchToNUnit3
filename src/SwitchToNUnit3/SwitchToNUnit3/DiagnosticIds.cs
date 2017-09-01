c6d1996b970609d17ac3b627eb1afbcf41d50cf1
David Störmer <badf00d@posteo.de>
2017-09-01 20:43:35 +0200

    Added test to verify issue #4.

--- src/SwitchToNUnit3/SwitchToNUnit3/DiagnosticIds.cs
+++ src/SwitchToNUnit3/SwitchToNUnit3/DiagnosticIds.cs
@@ -2,8 +2,9 @@
 {
     public static class DiagnosticIds
     {
-        public const string ExpectedExceptionAttributeisDeprecated = "STN001";
-        public const string ReferencedTestCaseSourceHasToBeStatic = "STN010";
-        public const string ThrowsIsDeprecated = "STN020";
+        public const string ExpectedExceptionAttributeisDeprecated = "STN3001";
+        public const string ReferencedTestCaseSourceHasToBeStatic = "STN3010";
+        public const string ThrowsIsDeprecated = "STN3020";
+        public const string AsyncVoidIsDeprected = "STN3030";
     }
 }
