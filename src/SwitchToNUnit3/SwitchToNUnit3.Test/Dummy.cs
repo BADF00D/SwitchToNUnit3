8c6dae340b4b90324083a49d6e9f994c243aee44
David Störmer <badf00d@posteo.de>
2017-09-01 19:59:28 +0200

    Cleared Dummy.cs

--- src/SwitchToNUnit3/SwitchToNUnit3.Test/Dummy.cs
+++ src/SwitchToNUnit3/SwitchToNUnit3.Test/Dummy.cs
@@ -1,13 +1,1 @@
-﻿using System;
-using NUnit.Framework;
-namespace Testnamespace {
-    public class SomeTest {
-        [ExpectedException]
-        public void SomeMethod() { }
-    }
-}
-namespace NUnit.Framework {
-    public class ExpectedExceptionAttribute : Attribute {
-        public ExpectedExceptionAttribute() { }
-    }
-}
+﻿
